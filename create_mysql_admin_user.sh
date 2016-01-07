4f7b56ca7f4b6d9776264fb39d912d60e38969de
bloodspasm <164440931@qq.com>
2016-01-07 14:45:03 +0800

    revert
    revert

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -10,18 +10,13 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
-# PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
-# _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
-# echo "=> Creating MySQL admin user with ${_word} password"
-
-# mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
-# mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
-PASS=''
+PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
 _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
-echo "=> Creating MySQL root user with ${_word} password"
+echo "=> Creating MySQL admin user with ${_word} password"
 
-mysql -uroot -e "CREATE USER 'root'@'%' IDENTIFIED BY '$PASS'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION"
+mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
+
 
 echo "=> Done!"
 
